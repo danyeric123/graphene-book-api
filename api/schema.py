65b2cfabbf896dc031713bc72f93a70e94d205ec
danyeric123 <dnagarpowers@gmail.com>
2021-10-03 15:10:08 -0400

    resolvers add

--- api/schema.py
+++ api/schema.py
@@ -1,7 +1,8 @@
 import graphene
 from graphene.types.field import Field
+from graphene.types.inputobjecttype import InputObjectType
 from graphene.types.objecttype import ObjectType
-from graphene.types.scalars import Int
+from graphene.types.scalars import ID, Int, String
 
 from graphene_django import DjangoObjectType, DjangoListField
 from .models import Book
@@ -20,3 +21,11 @@ class Query(ObjectType):
   
   def resolve_book(self,info, book_id):
     return Book.objects.get(pk=book_id)
+  
+class BookInput(InputObjectType):
+  id = ID()
+  title = String()
+  author = String()
+  year_published= String()
+  review = Int()
+  
