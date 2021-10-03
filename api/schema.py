b37061aeed137376243222dbef304353d59bfe98
danyeric123 <dnagarpowers@gmail.com>
2021-10-03 15:27:58 -0400

    basic mutations add

--- api/schema.py
+++ api/schema.py
@@ -1,6 +1,7 @@
 import graphene
 from graphene.types.field import Field
 from graphene.types.inputobjecttype import InputObjectType
+from graphene.types.mutation import Mutation
 from graphene.types.objecttype import ObjectType
 from graphene.types.scalars import ID, Int, String
 
@@ -29,3 +30,57 @@ class BookInput(InputObjectType):
   year_published= String()
   review = Int()
   
+class CreateBook(Mutation):
+  class Arguments:
+    book_data = BookInput(required=True)
+    
+  book = Field(BookType)
+  
+  @staticmethod
+  def mutate(root, info, book_data=None):
+    book_instance =Book.objects.create(
+      title=book_data.title,
+      author= book_data.author,
+      year_published=book_data.year_published,
+      review = book_data.review
+    )
+    return CreateBook(book=book_instance)
+  
+class UpdateBook(Mutation):
+  class Arguments:
+    book_data = BookInput(required=True)
+    
+  book = Field(BookType)
+  
+  @staticmethod
+  def mutate(root,info,book_data=None):
+    
+    book_instance = Book.objects.get(pk=book_data.id)
+    
+    if book_instance:
+      book_instance.title = book_data.title
+      book_instance.author = book_data.author
+      book_instance.year_published = book_data.year_published
+      book_instance.review = book_data.review
+      book_instance.save()
+      
+      return UpdateBook(book=book_instance)
+    return UpdateBook(book=None)
+  
+class DeleteBook(Mutation):
+  class Arguments:
+    id = ID()
+    
+  book = Field(BookType)
+  
+  @staticmethod
+  def mutate(root,info, id):
+    book_instance = Book.objects.get(pk=id)
+    book_instance.delete()
+    
+    return None
+    
+class Mutation(ObjectType):
+  create_book = CreateBook.Field()
+  update_book = UpdateBook.Field()
+  delete_book = DeleteBook.Field()
